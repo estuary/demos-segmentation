ab46c56e511792dc091f559819077b80a40c4546
Alex Burkhart <alex@estuary.dev>
2021-07-27 16:03:25 -0400

    Update tables that need to be reset

--- queries/pg_reset_tables.sql
+++ queries/pg_reset_tables.sql
@@ -1,4 +1,4 @@
-drop table flow_materializations;
-drop table gazette_checkpoints;
+drop table flow_materializations_v2;
+drop table flow_checkpoints_v1;
 drop table segment_memberships;
 drop table segment_profiles;
